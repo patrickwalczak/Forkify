edadce37e4b5731561fa67a3d9ccafacf4809c9e
patrickwalczak <pwalczakx@gmail.com>
2021-08-27 08:38:56 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '59fdfc85-3acd-4297-aac7-9adfbd009fae';
+export const MODAL_CLOSE_SEC = 2.5;
